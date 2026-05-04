429f0183eaae9f056c8e5c159461ea2e298eafb9
Yago Garzon Chaves <yagogarzon@gmail.com>
2026-05-04 20:46:21 -0300

    Dto Paciente

---
+++ src/back/src/main/java/psihub/dtos/CreatePatientDto.java
@@ -0,0 +1,11 @@
+package psihub.dtos;
+
+import java.time.LocalDate;
+
+public class CreatePatientDto {
+    String name;
+    LocalDate birthDate;
+    String cpf;
+    String telephone;
+    String email;
+}
---
+++ src/back/src/main/java/psihub/dtos/EditPatientDto.java
@@ -0,0 +1,11 @@
+package psihub.dtos;
+
+import java.time.LocalDate;
+
+public class EditPatientDto {
+    String name;
+    LocalDate birthDate;
+    String cpf;
+    String telephone;
+    String email;
+}
